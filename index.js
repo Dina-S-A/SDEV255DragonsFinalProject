231164f0879f71c6405ebee039c464d881563b6d
Dina-S-A <87444987+Dina-S-A@users.noreply.github.com>
2023-10-02 18:50:03 +0000

    Initializing GitHub

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const app = express();
+const test = "test";
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    res.render('index', { title: 'My Express App' });
+  res.render('index', { title: 'My Express App' });
 });
 app.listen(3000, () => {
-    console.log('Server started on port 3000');
+  console.log('Server started on port 3000');
 });
 
 app.post('/display-input', (req, res) => {
-    const input = req.body.hello;
-    res.render('input', { input });
+  const input = req.body.hello;
+  res.render('input', { input });
 });
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/input', (req, res) => {
-    res.render('input');
+  res.render('input');
 });
